39240215471905090c73685110c078b5c5a30dd3
frederico <frederico@mdlsistemas.com.br>
2015-10-29 20:12:09 -0200

    fixing regular expresion for active model

--- lib/app/models/magic_column.rb
+++ lib/app/models/magic_column.rb
@@ -3,15 +3,15 @@ class MagicColumn < ActiveRecord::Base
   has_many :owners, :through => :magic_column_relationships, :as => :owner
   has_many :magic_options
   has_many :magic_attributes, :dependent => :destroy
-  
+
   validates_presence_of :name, :datatype
-  validates_format_of :name, :with => /^[a-z][a-z0-9_]+$/
-  
+  validates_format_of :name, :with => /\A[a-z][a-z0-9_]+\z/
+
   def type_cast(value)
     begin
       case datatype.to_sym
         when :check_box_boolean
-          (value.to_int == 1) ? true : false 
+          (value.to_int == 1) ? true : false
         when :date
           Date.parse(value)
         when :datetime
@@ -25,10 +25,10 @@ class MagicColumn < ActiveRecord::Base
       value
     end
   end
-  
+
   # Display a nicer (possibly user-defined) name for the column or use a fancified default.
   def pretty_name
     super || name.humanize
   end
-  
+
 end
